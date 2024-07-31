3f510b0ad4587f7c4ca8ec57c13a38a81505d67b
lavishay <lavishay@campus.technion.ac.il>
2024-07-31 22:51:19 +0300

    UpdatingGit

--- Testing.sh
+++ Testing.sh
@@ -4,7 +4,7 @@ echo
 echo "" > 1.txt
 for i in $(ls -Ra);
 do
-    if printf "%s\n" "${EXCLUDE_PATHS_FILES[@]}" | grep -q -x "$i"
+    if printf "%s\n" "${EXCLUDE_PATHS_FILES[@]}" | grep -q "$i"
     then
         continue
     else
