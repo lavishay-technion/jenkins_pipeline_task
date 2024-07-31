a5ed1a3e0081d76fae6525f8525a76b13aba00a5
lavishay <lavishay@campus.technion.ac.il>
2024-08-01 00:39:22 +0300

    UpdatingGit

--- scripts/spellcheck.sh
+++ scripts/spellcheck.sh
@@ -49,7 +49,7 @@ do
     cat $i &> /dev/null
     if [[ $? == '0' ]]; then
         printf "\n\n####################\nFile %s \n####################\n" $i
-        hunspell -u -d en_US $i
+        hunspell -u -d en_US $i >> spell_check_report.txt
     fi
     printf "\n\n\n\n"
 done
