050050b7b8697e5de4bd3351725c3557d3c2b6b9
lavishay <lavishay@campus.technion.ac.il>
2024-08-02 16:57:00 +0300

    UpdatingGit

--- scripts/0_1_spellcheck_pre.sh
+++ scripts/0_1_spellcheck_pre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-##  This Script runs spell check for all Python files in the details-app project
+##  This Script runs spell check for all text containing files in the details-app project
 ##  Author: Avishay Layani
 ##  Collaborator: Ori Nahum
 ################################################################################
@@ -10,35 +10,46 @@
 
 . /etc/os-release ## Loading variables in that file to RAM, to check $ID content
 
-### Checking if the OS is Debian, Rocky or Alpine and running installations for hunspell accordingly
-if [[ $ID = "debian" || $ID = "ubuntu" ]]
+#######################################################
+### Checking hunspell installation
+#######################################################
+
+hunspell --version 2> /dev/null ## Checking if hunspell is installed
+if [[ $? != 0 ]]; ## If hunspell is not installed, install hunspell according to the correct OS
 then
-    sudo apt-get update
-    sudo apt-get install -y hunspell
-    echo "#######################################"
-    echo "[+] hunspell installed on Debian"
-    echo "#######################################"
-elif [[ $ID = "rocky" ]]
-then
-    sudo dnf update -y
-    sudo dnf install hunspell -y
-    echo "#######################################"
-    echo "[+] hunspell installed on Rocky"
-    echo "#######################################"
-elif [[ $ID = 'alpine' ]]
-then
-    apk add hunspell 
-    echo "#######################################"
-    echo " "
-    echo "[+] hunspell installed on Alpine"
-    echo "#######################################"
-else
-    echo "#######################################"
-    printf "[!] OS %s Is not compatible with this pipeline. \n[!] This is meant for Debian, Rocky or Alpine systems ONLY\n" $ID
-    echo "#######################################"
-    exit 1
+    ### Checking if the OS is Debian, Rocky or Alpine and running installations for hunspell accordingly
+    if [[ $ID = "debian" || $ID = "ubuntu" ]]
+    then
+        sudo apt-get update
+        sudo apt-get install -y hunspell
+        echo "#######################################"
+        echo "[+] hunspell installed on Debian"
+        echo "#######################################"
+    elif [[ $ID = "rocky" ]]
+    then
+        sudo dnf update -y
+        sudo dnf install hunspell -y
+        echo "#######################################"
+        echo "[+] hunspell installed on Rocky"
+        echo "#######################################"
+    elif [[ $ID = 'alpine' ]]
+    then
+        apk add hunspell 
+        echo "#######################################"
+        echo " "
+        echo "[+] hunspell installed on Alpine"
+        echo "#######################################"
+    else
+        echo "#######################################"
+        printf "[!] OS %s Is not compatible with this pipeline. \n[!] This is meant for Debian, Rocky or Alpine systems ONLY\n" $ID
+        echo "#######################################"
+        exit 1
+    fi
 fi
 
+#######################################################
+## Checking GIT installation
+#######################################################
 
 git --version 2> /dev/null  ## Checking if GIT is installed
 
@@ -74,6 +85,10 @@ then
 else
     echo "[+] GIT is already installed"
 fi
+
+#######################################################
+### Cloning Repo
+#######################################################
 
 ### Checking if the GIT repo was already cloned on that worker
 if [[ ! -d "/tmp/details_app"  ]]; then  ## If folder not found, clone it
