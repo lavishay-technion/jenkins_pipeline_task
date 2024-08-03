97367c1a2982360c6086b8e8a4dc652236b39b6d
lavishay <lavishay@campus.technion.ac.il>
2024-08-03 11:43:51 +0300

    UpdatingGit

---
+++ reports/codespell.md
@@ -0,0 +1,3 @@
+##############
+/tmp/details_app/pgsql/mistake_here:4: scrips ==> scripts
+/tmp/details_app/pgsql/mistake_here:6: ths ==> the, this
--- scripts/1_2_codespell&shell_exec.sh
+++ scripts/1_2_codespell&shell_exec.sh
@@ -4,40 +4,31 @@
 ##  This Script runs spell check for all Python files in the details-app project
 ##  Author: Avishay Layani
 ##  Collaborator: Ori Nahum
-##  Packages needed: hunspell 
+##  Packages needed: codespell, shellcheck, python3-pip(For Rocky/Alpine), apt-utils(For Debian)
 #################################################################################
 
 ##################################################################################################
 #   This script depends on the installation script with cloning the GIT
-#   It runs hunspell on every file under on the environment variable that comes from the Jenkins file
-#   A report will be saved in /home/reports and it's volumed to the VM at Jenkins_pipeline_task/docker/reports as spell_check_report
-##################################################################################################
-
-
-#!/bin/bash
-
-#################################################################################
-##  This Script runs spell check for all Python files in the details-app project
-##  Author: Avishay Layani
-##  Collaborator: Ori Nahum
-##  Packages needed: hunspell 
-#################################################################################
-
-##################################################################################################
-#   This script depends on the installation script with cloning the GIT
-#   It runs hunspell on every file under on the environment variable that comes from the Jenkins file
-#   A report will be saved in /home/reports and it's volumed to the VM at Jenkins_pipeline_task/docker/reports as spell_check_report
+#   It runs codespell and shellcheck on every file under on the environment variable that comes from the Jenkins file
+#   A report will be saved in /home/reports and it's volumed to the VM at Jenkins_pipeline_task/docker/reports as codespell.md & shellcheck.md
 ##################################################################################################
 
 
 
-#### Changing ownership of the reports folder, as it's in Volume, it's created with root permissions
+#### Changing ownership of the reports folder, as it's in Volume in docker compose, it's created with root permissions
 sudo chown -R jenkins:jenkins /home/reports
 
 
-### Stating first line of the file, cleaning it from past scripts' data.
+### Stating first line of the report files, cleaning it from past scripts' data.
 echo "##############" > /home/reports/codespell.md
-### Running codespell recursively on all files that end with .sh or .py in the application's directory
+echo "##############" > /home/reports/shellcheck.md
+### Running codespell recursively on all files that end with what is entered in SCRIPTS_EXT in the application's directory
 codespell $APP_DIR $SCRIPTS_EXT >> /home/reports/codespell.md
 
-echo "##############"
+for i in $(find $APP_DIR)  ## Running on every file in the app's dir
+do
+    if [[ $i =~ ".sh" ]]; then ## Checking if it's a bash script
+        shellcheck $i >> /home/reports/shellcheck.md ## If yes, running shellcheck on it, and sending it to a report
+
+echo "##############" >> /home/reports/codespell.md
+echo "##############" >> /home/reports/shellcheck.md
