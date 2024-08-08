3bf2226955c30f766872a8f4a402ef76b9320605
lavishay <lavishay@campus.technion.ac.il>
2024-08-08 22:28:30 +0300

    fixing codespell

--- scripts/02_exec_codespell.sh
+++ scripts/02_exec_codespell.sh
@@ -8,6 +8,13 @@
 if [[ ! -d $OUTPUT_DIR ]]; then
     mkdir $OUTPUT_DIR
 fi
+##Creating files with typos
+echo lable > $APP_DIR/file_with_typos.txt
+echo devic > $APP_DIR/file_with_typos.txt
+echo varning > $APP_DIR/file_with_typos2.txt
+echo implemen > $APP_DIR/file_with_typos2.txt
+echo nd > $APP_DIR/file_with_typos1.txt
+echo suppor > $APP_DIR/file_with_typos1.txt
 
 #### Changing ownership of the reports folder, as it's in Volume, it's created with root permissions
 sudo chown -R jenkins:jenkins "${OUTPUT_DIR}"
@@ -18,14 +25,16 @@ echo "CodeSpell Results" >> "${OUTPUT_DIR}/02_codespell_results.md"
 echo "==================" >> "${OUTPUT_DIR}/02_codespell_results.md"
 
 # Loop through all directories specified in PATHS_LOCATION and find .sh files
-for LOCATION in "${APP_DIR[@]}"; do
-    if [[ $LOCATION != '' ]]; then
-        # Find .sh files in the current location and run codespell on them
-        find $LOCATION -name "*.sh" -exec codespell {} + >> "${OUTPUT_DIR}/02_codespell_results.md"
-    else
-        echo "[-] Location was not provided"
-    fi
-done
+codespell -c $APP_DIR >> "${OUTPUT_DIR}/02_codespell_results.md"
+
+# for LOCATION in "${APP_DIR[@]}"; do
+#     if [[ $LOCATION != '' ]]; then
+#         # Find .sh files in the current location and run codespell on them
+#         find $LOCATION -name "*.sh" -exec codespell {} + >> "${OUTPUT_DIR}/02_codespell_results.md"
+#     else
+#         echo "[-] Location was not provided"
+#     fi
+# done
 
 # Notify the user that the results have been saved
 echo "[v] Code Spell Results saved in ${OUTPUT_DIR}/02_codespell_results.md"
